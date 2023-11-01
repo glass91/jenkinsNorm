c107b4781a0c76d220fc4fd89ae5f142d1570b16
glass91@gmail.com <glass91@gmail.com>
2023-11-01 14:13:08 -0400

    bbb

--- terraform_ansible_generic_instace_setup_template/main.tf
+++ terraform_ansible_generic_instace_setup_template/main.tf
@@ -13,11 +13,11 @@ terraform {
   required_version = ">= 1.3"
   backend "s3" {
     bucket = "terraform-state-test-my-cloud"
-    key    = "nodejsapp_example/terraform.tfstate"
+    key    = "jenkins-ansible/terraform.tfstate"
     region = "us-east-1"
   }
 }
 
 provider "aws" {
   region     = "us-east-1"
-}
+}    
